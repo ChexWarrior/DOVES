a452616d812261ee49e73284e5171a1ad52a6c93
erek.korgan <erek.korgan@d6a80ec4-4081-5d54-fe7a-abef245e88c1>
2013-03-06 20:42:30 +0000

    Added the check for "yes" or "no" to bird models/bird.rb, along with checks for empty fields
    removed destroy button from birds/index.html.erb
    
    next im going to add the toggle from reviewable to unreviewable and vice versa

---  doves/app/models/bird.rb
+++  doves/app/models/bird.rb
@@ -1,2 +1,18 @@
 class Bird < ActiveRecord::Base
+
+validates :common_name, :presence => true, :uniqueness => true
+validates :reviewable, :presence => true
+validate :yesorno
+
+#Right now we have a text box for the reviewable field
+#where the admin would either put "yes" or "no" - the 
+#function below checks for that. But later, I want to 
+#just make it a check box instead. - Erek
+
+def yesorno
+	if :reviewable.downcase != "yes" || :reviewable.downcase != "no"
+		errors.add(:reviewable, "must be either \"yes\" or \"no\"")
+	end
+end
+
 end
