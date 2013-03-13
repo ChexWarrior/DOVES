96ea3a79d7b048b23e80686d2fcae8148991785e
erek.korgan <erek.korgan@d6a80ec4-4081-5d54-fe7a-abef245e88c1>
2013-03-13 19:21:35 +0000

    added radio buttons, toggle button not working properly just yet but i added buttons

--- trunk/ doves/app/models/bird.rb
+++ trunk/ doves/app/models/bird.rb
@@ -4,15 +4,20 @@ validates :common_name, :presence => true, :uniqueness => true
 validates :reviewable, :presence => true
 validate :yesorno
 
-#Right now we have a text box for the reviewable field
-#where the admin would either put "yes" or "no" - the 
-#function below checks for that. But later, I want to 
-#just make it a check box instead. - Erek
 
 def yesorno
 	if reviewable.downcase != "yes" && reviewable.downcase != "no"
 		errors.add(:reviewable, "must be either \"yes\" or \"no\"")
 	end
+end
+
+def toggle
+	if reviewable.downcase == "yes"
+		reviewable = "no"
+	else 
+		reviewable = "yes"
+	end
+	return true
 end
 
 end
