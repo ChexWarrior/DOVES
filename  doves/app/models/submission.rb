0bcb961f4f158fa8f476b5e5a9d911b03c579819
wbish001@odu.edu <wbish001@odu.edu@d6a80ec4-4081-5d54-fe7a-abef245e88c1>
2013-03-13 20:47:39 +0000

    search template.  Conditions and variables need further definition

---  doves/app/models/submission.rb
+++  doves/app/models/submission.rb
@@ -7,3 +7,8 @@ def created_on
 end
 
 end
+
+def self.search(search)
+  search_condition = "%" + search + "%"
+  find(:all, :conditions => ['title LIKE ? OR description LIKE ?', search_condition, search_condition])
+end
