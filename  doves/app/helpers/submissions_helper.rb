1d5c0951f7c085262d68027a8093b606efd59407
erek.korgan <erek.korgan@d6a80ec4-4081-5d54-fe7a-abef245e88c1>
2013-03-24 19:38:15 +0000

    added helper

---  doves/app/helpers/submissions_helper.rb
+++  doves/app/helpers/submissions_helper.rb
@@ -1,2 +1,11 @@
 module SubmissionsHelper
+
+def search_params
+    [
+      ['First Name', 'first_name'],
+      ['Last Name', 'last_name'],
+      ['Email', 'email'],
+      ['Common Name', 'common_name']
+    ]
+end
 end
