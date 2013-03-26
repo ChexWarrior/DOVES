a6188190ffc1fe1748cf394234ddc6dc45ba5355
achin001@odu.edu <achin001@odu.edu@d6a80ec4-4081-5d54-fe7a-abef245e88c1>
2013-03-26 01:05:14 +0000

    Added image upload fields to submission form.

---  doves/app/models/multimedium.rb
+++  doves/app/models/multimedium.rb
@@ -1,5 +1,5 @@
 class Multimedium < ActiveRecord::Base
-	attr_accessible :Submission_id, :type, :time, :image, :size
+	attr_accessible :submission_id, :type, :time, :image, :size
 	belongs_to :submission
 	mount_uploader :image, ImageUploader
 	
