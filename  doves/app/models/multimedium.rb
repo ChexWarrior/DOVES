752df024cf0fb69baafbe7cd01516cb8b8813109
achin001@odu.edu <achin001@odu.edu@d6a80ec4-4081-5d54-fe7a-abef245e88c1>
2013-03-27 03:16:41 +0000

    Existing submissions show attached images.

---  doves/app/models/multimedium.rb
+++  doves/app/models/multimedium.rb
@@ -1,5 +1,5 @@
 class Multimedium < ActiveRecord::Base
-	attr_accessible :submission_id, :type, :time, :image, :size
+	#attr_accessible :submission_id, :image
 	belongs_to :submission, :counter_cache => :media
 	mount_uploader :image, ImageUploader
 	
