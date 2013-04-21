38fb0ac5f9d1e5cb624ae40763f59e707208138b
amir.sufyani <amir.sufyani@d6a80ec4-4081-5d54-fe7a-abef245e88c1>
2013-04-21 20:27:57 +0000

    Stuff

---  doves/app/controllers/votes_controller.rb
+++  doves/app/controllers/votes_controller.rb
@@ -68,7 +68,7 @@ before_filter :ensure_reviewer_or_admin
 	
     respond_to do |format|
       if @vote.save
-        format.html { redirect_to @vote, notice: 'Vote was successfully created.' }
+        format.html { redirect_to @vote.submission, notice: 'Vote was successfully created.' }
         format.json { render json: @vote, status: :created, location: @vote }
       else
         format.html { render action: "new" }
