bd0b51fb5fe74a743b93b1c6ec09dce41a935db4
amir.sufyani <amir.sufyani@d6a80ec4-4081-5d54-fe7a-abef245e88c1>
2013-04-21 19:44:14 +0000

    LOLS

--- trunk/ doves/app/models/submission.rb
+++ trunk/ doves/app/models/submission.rb
@@ -7,28 +7,28 @@ accepts_nested_attributes_for :multimedia
 belongs_to :bird
 belongs_to :user
 
-# validates :common_name, :presence => true
-# validates :s_degree, :presence => true
-# validates :age, :presence => true
-# validates :sex, :presence => true
-# validates :plumage, :presence => true
-# validates :location, :presence => true
-# validates :region, :presence => true
-# validates :sight_date_time, :presence => true
-# validates :habitat, :presence => true
-# validates :gps, :presence => true
-# validates :coobservers, :presence => true
-# validates :length_of_obs, :presence => true
-# validates :distance_from, :presence => true
-# validates :detailed_description, :presence => true
-# validates :detailed_behavior, :presence => true
-# validates :distinguished_char, :presence => true
-# validates :prev_bird_exp, :presence => true
-# validates :optical_equipment, :presence => true
-# validates :references, :presence => true
-# validates :sub_recall, :presence => true
-# validates :guide_use, :presence => true
-# validates :unusual, :presence => true
+ validates :common_name, :presence => true
+ validates :s_degree, :presence => true
+ validates :age, :presence => true
+ validates :sex, :presence => true
+ validates :plumage, :presence => true
+ validates :location, :presence => true
+ validates :region, :presence => true
+ validates :sight_date_time, :presence => true
+ validates :habitat, :presence => true
+ validates :gps, :presence => true
+ validates :coobservers, :presence => true
+ validates :length_of_obs, :presence => true
+ validates :distance_from, :presence => true
+ validates :detailed_description, :presence => true
+ validates :detailed_behavior, :presence => true
+ validates :distinguished_char, :presence => true
+ validates :prev_bird_exp, :presence => true
+ validates :optical_equipment, :presence => true
+ validates :references, :presence => true
+ validates :sub_recall, :presence => true
+ validates :guide_use, :presence => true
+ validates :unusual, :presence => true
 
 def self.subsearch(search, option, status)
   search_condition = "%" + search + "%"
