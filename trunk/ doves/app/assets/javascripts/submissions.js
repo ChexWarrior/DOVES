22817adab22658a3b3009c16185354ecb2b6c726
achin001@odu.edu <achin001@odu.edu@d6a80ec4-4081-5d54-fe7a-abef245e88c1>
2013-05-09 01:28:54 +0000

    Added client side and model level file size validations. Edited submission form to show limits.

--- trunk/ doves/app/assets/javascripts/submissions.js
+++ trunk/ doves/app/assets/javascripts/submissions.js
@@ -2,3 +2,80 @@
 $(document).ready(function() {
 	$(".datep").datepicker();
 });
+
+function validateImage(inputFile) {
+  var maxExceededMessage = "This file exceeds the maximum allowed file size (5 MB)";
+  var extErrorMessage = "Only image files with extension: .jpg, .jpeg, .gif, .bmp or .png are allowed";
+  var allowedExtension = ["jpg", "jpeg", "gif", "png", "bmp"];
+ 
+  var extName;
+  var maxFileSize = $(inputFile).data('max-file-size');
+  var sizeExceeded = false;
+  var extError = false;
+ 
+  $.each(inputFile.files, function() {
+    if (this.size && maxFileSize && this.size > parseInt(maxFileSize)) {sizeExceeded=true;};
+    extName = this.name.split('.').pop().toLowerCase();
+    if ($.inArray(extName, allowedExtension) == -1) {extError=true;};
+  });
+  if (sizeExceeded) {
+    window.alert(maxExceededMessage);
+    $(inputFile).val('');
+  };
+ 
+  if (extError) {
+    window.alert(extErrorMessage);
+    $(inputFile).val('');
+  };
+}
+function validateVideo(inputFile) {
+  var maxExceededMessage = "This file exceeds the maximum allowed file size (15 MB)";
+  var extErrorMessage = "Only video files with extension: .mpg, .avi, .mp4, .mov, .mpeg or .wmv are allowed";
+  var allowedExtension = ["mpg", "avi", "mp4", "mov", "mpeg", "wmv"];
+ 
+  var extName;
+  var maxFileSize = $(inputFile).data('max-file-size');
+  var sizeExceeded = false;
+  var extError = false;
+ 
+  $.each(inputFile.files, function() {
+    if (this.size && maxFileSize && this.size > parseInt(maxFileSize)) {sizeExceeded=true;};
+    extName = this.name.split('.').pop().toLowerCase();
+    if ($.inArray(extName, allowedExtension) == -1) {extError=true;};
+  });
+  if (sizeExceeded) {
+    window.alert(maxExceededMessage);
+    $(inputFile).val('');
+  };
+ 
+  if (extError) {
+    window.alert(extErrorMessage);
+    $(inputFile).val('');
+  };
+}
+function validateAudio(inputFile) {
+  var maxExceededMessage = "This file exceeds the maximum allowed file size (5 MB)";
+  var extErrorMessage = "Only audio files with extension: .wav or .mp3 are allowed";
+  var allowedExtension = ["wav", "mp3"];
+ 
+  var extName;
+  var maxFileSize = $(inputFile).data('max-file-size');
+  var sizeExceeded = false;
+  var extError = false;
+ 
+  $.each(inputFile.files, function() {
+    if (this.size && maxFileSize && this.size > parseInt(maxFileSize)) {sizeExceeded=true;};
+    extName = this.name.split('.').pop().toLowerCase();
+    if ($.inArray(extName, allowedExtension) == -1) {extError=true;};
+  });
+  if (sizeExceeded) {
+    window.alert(maxExceededMessage);
+    $(inputFile).val('');
+  };
+ 
+  if (extError) {
+    window.alert(extErrorMessage);
+    $(inputFile).val('');
+  };
+}
+
--- trunk/ doves/app/models/multimedium.rb
+++ trunk/ doves/app/models/multimedium.rb
@@ -1,9 +1,24 @@
 class Multimedium < ActiveRecord::Base
-	#attr_accessible :submission_id, :image
+
 	belongs_to :submission, :counter_cache => :media
 	mount_uploader :image, ImageUploader
 	mount_uploader :video, VideoUploader
 	mount_uploader :audio, AudioUploader
+	
+	validate :image_size_validation
+	validate :video_size_validation
+	validate :audio_size_validation
 		
 	after_save { |multimedium| multimedium.destroy if multimedium.image.blank? and multimedium.video.blank? and multimedium.audio.blank? }
+
+	
+	def image_size_validation
+		errors[:image] << "should be less than 5MB" if image.size > 5.megabytes
+	end
+	def video_size_validation
+		errors[:video] << "should be less than 15MB" if video.size > 15.megabytes
+	end
+	def audio_size_validation
+		errors[:audio] << "should be less than 5MB" if audio.size > 5.megabytes
+	end
 end
